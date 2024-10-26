01c40693e7b1ec42c086227b03d9e088a41463b8
mateo <mateo.ossa.b@gmail.com>
2024-10-25 23:49:43 -0300

    added a new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,13 +11,16 @@ const Projects = () => {
           <p>game hub application, all games coded manually using matrixes</p>
           <a href="https://00mateo00.github.io/games-app/">got to project →</a>
         </div>
-        {[1, 2].map((item) => (
-          <div className="project-card" key={item}>
-            <h3>Project {item}</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <a href="#">Learn more →</a>
-          </div>
-        ))}
+        <div className="project-card">
+          <h3>GTD</h3>
+          <p>GTD application, originally made using springboot as the backend</p>
+          <a href="https://00mateo00.github.io/GTD_project/">got to project →</a>
+        </div>
+        <div className="project-card">
+          <h3>Project 3</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <a href="#">Learn more →</a>
+        </div>
       </div>
     </section>
   );
