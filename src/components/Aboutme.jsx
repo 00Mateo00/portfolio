dbd78cc332c5ece2f8acde7cd37761999d9f6278
mateo <mateo.ossa.b@gmail.com>
2024-10-25 23:23:24 -0300

    improvements to the responsiveness

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -6,11 +6,14 @@ const Aboutme = () => {
     <section id="about" className="about">
       <h2 className="section__title">About Me</h2>
       <div className="about__content">
-        I am passionate about programming, entrepreneurship, nutrition, and physics, with a strong focus on continuous improvement. I enjoy solving problems, creating innovative
-        solutions, and learning new skills. Applying my knowledge to contribute value to diverse projects excites me. What sets me apart is my dedication to self-improvement and
-        adaptability to new challenges. My blend of programming skills, entrepreneurial mindset, my bilingual proficiency in English and Spanish enables me to offer unique, innovative
-        solutions. I am proactive, curious, and always eager to learn and grow. I am seeking a role where I can leverage my coding expertise and English proficiency. I look forward to
-        tackling challenges that promote continuous learning, enhance my abilities, and explore new technologies, algorithms, and methodologies to fuel my professional growth.
+        <p>
+          I am passionate about programming, entrepreneurship, nutrition, and physics, with a strong focus on continuous improvement. I enjoy solving problems, creating innovative
+          solutions, and learning new skills. Applying my knowledge to contribute value to diverse projects excites me. What sets me apart is my dedication to self-improvement and
+          adaptability to new challenges. My blend of programming skills, entrepreneurial mindset, my bilingual proficiency in English and Spanish enables me to offer unique,
+          innovative solutions. I am proactive, curious, and always eager to learn and grow. I am seeking a role where I can leverage my coding expertise and English proficiency. I
+          look forward to tackling challenges that promote continuous learning, enhance my abilities, and explore new technologies, algorithms, and methodologies to fuel my
+          professional growth.
+        </p>
       </div>
     </section>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,12 @@ const Projects = () => {
     <section className="projects">
       <h2>Featured Projects</h2>
       <div className="projects-grid">
-        {[1, 2, 3].map((item) => (
+        <div className="project-card">
+          <h3>GamesApp</h3>
+          <p>game hub application, all games coded manually using matrixes</p>
+          <a href="https://00mateo00.github.io/games-app/">got to project →</a>
+        </div>
+        {[1, 2].map((item) => (
           <div className="project-card" key={item}>
             <h3>Project {item}</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
